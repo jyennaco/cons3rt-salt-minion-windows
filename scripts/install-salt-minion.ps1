870cd08a9fce0db339ade2e48bcee37e142918c1
Joe Yennaco <joe@joeyennaco.com>
2014-02-27 22:58:20 -0500

    Updated powershell script to look for salt-master in fap-deployment.properties, and attempt to install Salt Minion if it find the salt-master.

--- scripts/install-salt-minion.ps1
+++ scripts/install-salt-minion.ps1
@@ -28,7 +28,7 @@ $DEPLOYMENT_HOME=$args[3]
 # Location of the INSTALLER if included.  If not included, this script will pull from the URL.
 $MEDIA_SHARE="$pwd\..\media"
 
-# Local variables for the log file
+# Directory and log file variables
 $TIMESTAMP=Get-Date -f yyyy-MM-dd-HHmm
 $ROOT="C:"
 $CONS3RT_DIRNAME="cons3rt"
@@ -69,7 +69,7 @@ function install-salt-minion-windows {
 		echo "### INFO: The media file $INSTALLER not found in $MEDIA_SHARE.  Attempting to download $INSTALLER from $URL ...`n"
 		$client = new-object System.Net.WebClient
 		$client.DownloadFile("$URL/$INSTALLER","$CONS3RT_DIR\$INSTALLER")
-		echo "### INFO: done.`n"
+		echo "### INFO: done downloading $INSTALLER.`n"
 	}
 	else {
 		echo "### INFO: The media file $INSTALLER was found in $MEDIA_SHARE, copying over to $CONS3RT_DIR ...`n"
@@ -77,94 +77,38 @@ function install-salt-minion-windows {
 		echo "### INFO: done.`n"
 	}
 	
-	# Exit if the value for ASSET_DIR is not set
-	if ( !$DEPLOYMENT_HOME ) {
-		echo "### INFO: DEPLOYMENT_HOME is not set. Cannot read salt-master from runtime properties.`n"
-	}
-	else {
-		echo "### INFO: DEPLOYMENT_HOME set."
-		
-		# Exit if the install media can't be found
-		if ( !(test-path $DEPLOYMENT_HOME\fap-deployment.properties) ) {
-			echo "### ERROR: DEPLOYMENT _HOME was set but the fap-deployment.properties file was not found in $DEPLOYMENT_HOME.  Cannot determine salt-master from runtime properties.  Salt Minion will not be installed, but can be installed manually from $CONS3RT_DIR.  Exiting ...`n"
-			exit
-		}
-		
-		
-		$saltMaster = Get-Content $propFile | Select-String "salt-master" | foreach {$d = $_ -split "="; Write-Output $d[1] }
-	}
-	
+	echo "### INFO: DEPLOYMENT_HOME: $DEPLOYMENT_HOME`n"
 	
 	# Location of Deployment properties file
 	$propFile="$DEPLOYMENT_HOME/fap-deployment.properties"
 	
-	
-	
-
-	
-
-	
-	# Create the install directories if they doesn't exist
-	echo "Checking if $INST_ROOT directory exists ... "
-	if ( !(test-path $INST_ROOT) ) {
-		echo "Creating the $INST_ROOT directory ... "
-		New-Item -name tools -path "$INST_ROOT\" -itemType directory -force
-		echo "done.`n"
+	# Try to get salt-master IP/hostname from the fap-deployment.properties file
+	if ( !$DEPLOYMENT_HOME -or !(test-path $propFile) ) {"
+		echo "### WARN: fap-deployment.properties file was not found in $DEPLOYMENT_HOME.  Cannot determine salt-master from role name or runtime properties."
+		echo "### WARN: Salt Minion will not be automatically installed, but can be manually installed from $CONS3RT_DIR after logging in."
 	}
 	else {
-		echo "The $INST_ROOT directory already exists.`n"
+		# Copy the fap-deployment.properties file to the $CONS3RT_DIR
+		echo "### INFO: Copying fap-deployment.properties file to $CONS3RT_DIR for inspection after logging in ..."
+		Copy-Item $propFile $CONS3RT_DIR\ -recurse -force
+		
+		# Grab the salt-master hostname or IP address if it exists
+		$saltMaster = Get-Content $propFile | Select-String "salt-master" | foreach {$d = $_ -split "="; Write-Output $d[1] }
+		
+		# Check to see if salt-master was specified.  Note that Powershell is case insensitive.
+		if ( !$saltMaster ) {
+			echo "### WARN: fap-deployment.properties did not contain an entry for salt-master (case insensitive), not defined as a role name or runtime property."
+			echo "### WARN: Salt Minion will not be automatically installed, but can be manually installed from $CONS3RT_DIR after logging in."
+		}
+		else {
+			echo "### INFO: fap-deployment.properties contains a runtime property for salt-master: $saltMaster"
+			echo "### INFO: Salt Minion for Windows will be installed with $saltMaster as the salt-master ..."
+			$rand=GetRandom
+			$minionName=minion-$rand
+			$CONS3RT_DIR\$INSTALLER /S /master=$saltMaster /minion-name=$minionName
+			echo "### INFO: done installing Salt Minion on Windows."
+		}
 	}
-	
-	########## CTE SERVER INSTALLATION ##########
-	
-	echo "Installing the CTE Web Server ... `n"
-	
-	# Exit if the install media can't be found
-	if ( !(test-path $MEDIA_SHARE\$DEVTOOL_INSTALLER) ) {
-		echo "### ERROR: The media file $DEVTOOL_INSTALLER not found in $MEDIA_SHARE.  Exiting...`n"
-		exit
-	}
-	
-	# Copying the CTE file to $INST_ROOT
-	echo "Copying the $MEDIA_SHARE\$DEVTOOL_INSTALLER directory to $INST_ROOT\ ... "
-	Copy-Item $MEDIA_SHARE\$DEVTOOL_INSTALLER $INST_ROOT\ -recurse -force
-	echo "done.`n"
-	
-	# Exit if the install media can't be found
-	if ( !(test-path $INST_ROOT\$DEVTOOL_INSTALLER) ) {
-		echo "### ERROR: The media file $DEVTOOL_INSTALLER not found in $INST_ROOT.  The file must not have copied correctly.  Exiting...`n"
-		exit
-	}
-	
-	# Unziping the CTE installer
-	echo "Unzipping the $INST_ROOT\$DEVTOOL_INSTALLER to $INST_ROOT\ ... "
-	$unzipSuccess=unzip "$INST_ROOT\$DEVTOOL_INSTALLER" "$INST_ROOT\"
-	
-	# Exit if unzipped was not successful
-	if ( $unzipSuccess -eq 0 ) {
-		echo "### ERROR: $DEVTOOL_INSTALLER was not able to be unzipped.  Exiting..."
-		exit
-	}
-	echo "done.`n"
-	
-	# Exit if the install media can't be found
-	if ( !(test-path $MEDIA_SHARE\$OPT_INSTALLER) ) {
-		echo "### ERROR: The media file $OPT_INSTALLER not found in $MEDIA_SHARE.  Exiting...`n"
-		exit
-	}
-	
-	# Copying the CTE opt directories to $INST_ROOT
-	echo "Copying the $MEDIA_SHARE\$OPT_INSTALLER directory to $INST_ROOT\ ... "
-	Copy-Item $MEDIA_SHARE\$OPT_INSTALLER $INST_ROOT\ -recurse -force
-	echo "done.`n"
-	
-	# Exit if the install media can't be found
-	if ( !(test-path $INST_ROOT\$OPT_INSTALLER) ) {
-		echo "### ERROR: The media file $OPT_INSTALLER not found in $INST_ROOT.  The file must not have copied correctly.  Exiting...`n"
-		exit
-	}
-	
-	
 	
 	echo "### INFO: Salt-Minion Install Script Complete.`n"
 }
